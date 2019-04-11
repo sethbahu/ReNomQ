7671818c13e53e97fe37cc4d4c5116c924ad9652
K.Shiba <s1513094@edu.cc.uec.ac.jp>
2019-04-11 22:18:07 +0900

    modify __init__.py

--- renom_q/__init__.py
+++ renom_q/__init__.py
@@ -1,4 +1,11 @@
-#from .core import *
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, Grid.
+#
+# This source code is licensed under the ReNom Subscription Agreement, version 1.0.
+# ReNom Subscription Agreement Ver. 1.0 (https://www.renom.jp/info/license/index.html)
+
+
 from .quantumregister import QuantumRegister
 from .classicalregister import ClassicalRegister
 from .quantumcircuit import QuantumCircuit
